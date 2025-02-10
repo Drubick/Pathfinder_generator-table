54ce540f53d1a160a5742db574600ccc1fc6c885
Valentin Nastase <93141743+Drubick@users.noreply.github.com>
2025-02-10 13:42:08 +0000

    dropdownmenu added

--- src/Menus/TopMenuButton.jsx
+++ src/Menus/TopMenuButton.jsx
@@ -1,8 +1,8 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const navItems = [
     {
-        title: "Random Generators", options: ["Encounter Generator asdasdyagfsudgauysdfausydfaiusydtfas", "Map Generator"], colapsed: true, 
+        title: "Random Generators", options: ["Encounter Generator", "Map Generator"], colapsed: true, 
     },
     {
         title: "Builders", options: ["Encounter Builders", "Map Builder"], colapsed: true
@@ -14,15 +14,30 @@ const navItems = [
 
 export default function TopMenuButton() {
     const [activeIndex, setActiveIndex] = useState(null);
+    const buttonRefs = useRef([]);
 
     const handleClick = (index) => {
         setActiveIndex(activeIndex === index ? null : index);
     };
 
+    useEffect(() => {
+        const handleDocumentClick = (event) => {
+            if (buttonRefs.current.every(ref => ref && !ref.contains(event.target))) {
+                setActiveIndex(null);
+            }
+        };
+
+        document.addEventListener('click', handleDocumentClick);
+        return () => {
+            document.removeEventListener('click', handleDocumentClick);
+        };
+    }, []);
+
+    
     return (
         <div className="flex space-x-2">
             {navItems.map((item, index) => {
-                let buttonClasses = "py-1 px-2 font-semibold text-center align-middle transition-all duration-300 " +
+                let buttonClasses = "py-1 drop-shadow-md px-2 font-semibold text-center align-middle transition-all duration-300 " +
                     "ease-in-out border-2 border-solid rounded cursor-pointer select-none hover:text-black " +
                     "dark:hover:text-white hover:border-accent-light dark:hover:border-accent-dark shadow-2xl text-xs";
 
@@ -31,13 +46,27 @@ export default function TopMenuButton() {
                 }
 
                 return (
-                    <button 
-                        key={index}
-                        onClick={() => handleClick(index)}
-                        className={buttonClasses}
-                    >
-                        {item.title}
-                    </button>
+                    <div key={index} ref={el => buttonRefs.current[index] = el}>
+                        <button 
+                            onClick={() => handleClick(index)}
+                            className={buttonClasses}
+                        >
+                            {item.title} <span className='ml-2'>{activeIndex === index ? '▲' : '▼'}</span>
+                        </button>
+                        {activeIndex === index && (
+                            <div className="absolute bg-pre-primary-light dark:bg-pre-primary-dark 
+                            border-0 border-solid rounded drop-shadow-md hover:cursor-pointer ">
+                                {item.options.map((option, index) => {
+                                    return (
+                                        <div key={index} className="px-2 py-1 hover:bg-accent-light-2 dark:hover:bg-accent-dark-2
+                                        ">
+                                            {option}
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        )}
+                    </div>
                 );
             })}
         </div>
