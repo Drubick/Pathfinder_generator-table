36948e16662a13f4bfbad1c507d9901d313277f0
Derrubick <42.vnastase@gmail.com>
2025-02-16 08:43:32 +0100

    removed an extra src file not deleted by git

--- src/ContentArea/ContentArea.jsx
+++
@@ -1,8 +0,0 @@
-import RandomCombatGenerator from "./RandomCombatGenerator";
-
-export default function ContentArea({ content }) {
-    return (
-    content === 'Combat Generator' && <RandomCombatGenerator />
-    
-    );
-}
--- src/UI/Button.jsx
+++
@@ -1,7 +0,0 @@
-export default function Button({text}){
-    return(
-        <button className=" shadow-lg hover:cursor-pointer  bg-tertiary-light
-        dark:bg-tertiary-dark hover:bg-tertiary-light-2 hover:dark:bg-tertiary-dark-2
-        py-1 px-2 border rounded">{text}</button>
-    )
-    }
